66f3bca16ef897f16101a0fa04ee4f479f93dd8a
Artjom Nikokošev <anikok@ttu.ee>
2023-06-01 13:14:03 +0300

    functional routes

--- src/main/java/project1/handler/GreetingHandler.java
+++ src/main/java/project1/handler/GreetingHandler.java
@@ -22,6 +22,15 @@ public class GreetingHandler {
     }
 
     public Mono<ServerResponse> flux(ServerRequest request) {
+        Long start = request.queryParam("start")
+                .map(Long::valueOf)
+                .orElse(0L);
+
+        Long count = request.queryParam("count")
+                .map(Long::valueOf)
+                .orElse(3L);
+
+
         Flux<Greeting> data = Flux
                 .just(
                         "Hello, reactive!",
@@ -29,7 +38,10 @@ public class GreetingHandler {
                         "Third post",
                         "Fourth post",
                         "Fifth post"
-                ).map(Greeting::new);
+                )
+                .skip(start)  // where we begin
+                .take(count) // how much elements we take
+                .map(Greeting::new);
 
         return ServerResponse
                 .ok()
